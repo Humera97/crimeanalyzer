95d096af363c51a8976d0bc7c52ca159458366ec
Humera97 <humeraslodhi@gmail.com>
2018-11-02 20:32:09 -0600

    Added crime categories

---
+++ categories.cs
@@ -0,0 +1,20 @@
+using System;
+namespace Crimanalyzer
+{
+    public class categories
+    {
+        public int year;
+        public int population;
+        public int violent;
+        public int murder;
+        public int rape;
+        public int robbery;
+        public int assault;
+        public int property;
+        public int burglary;
+        public int theft;
+        public int vehicle;
+      
+    }
+}
+
